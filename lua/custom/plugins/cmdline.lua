e41b660360dd4c901aee8b3c852740029b7da626
blithe <cn18750441251@126.com>
2023-05-24 21:59:06 +0800

    add find-cmdline, beautiful floating cmdline

---
+++ lua/custom/plugins/cmdline.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    -- beautiful command line
+    'VonHeikemen/fine-cmdline.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim'
+    },
+    config = function()
+      vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {noremap = true})
+    end
+  }
+}
