e0690a5897c628877bbad74ec55e969d185ed2e9
blithe <cn18750441251@126.com>
2023-05-26 16:57:49 +0800

    add which-key timeout
    add timeout to provide more time to press combo keys
    especially `gU` in vim-fugitive

--- init.lua
+++ init.lua
@@ -61,7 +61,13 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 700
+    end,
+    opts = {},
+  },
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
