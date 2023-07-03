2041c4a4320448b480257c781d9eb60b750d85c1
blithe <cn18750441251@126.com>
2023-07-03 17:59:23 +0800

    disable null-ls when tsserver enabled
    see: https://www.reddit.com/r/neovim/comments/xmzm01/how_to_disable_nullls_if_a_certain_lsp_server_is

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,7 +47,15 @@ lsp.on_attach(function(client, bufnr)
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
+    vim.lsp.buf.format {
+      -- disable null-ls is tsserver is attached...
+      filter = function(c)
+        if c.name == "null-ls" and #vim.lsp.get_active_clients { name = "tsserver", bufnr = 0 } == 0 then
+          return true
+        end
+        return c.name == "tsserver"
+      end,
+    }
   end, { desc = 'Format current buffer with LSP' })
 end)
 
