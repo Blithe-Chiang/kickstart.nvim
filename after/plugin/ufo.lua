eed49ddbd92489e7e5e7326db483c0f7931c6e11
blithe <cn18750441251@126.com>
2024-03-11 20:55:10 +0800

    Revert "add folding plugin"
    This reverts commit f280288447a0b703ab66a97dbe42b01da7985e1b.

--- after/plugin/ufo.lua
+++
@@ -1,7 +0,0 @@
-require('ufo').setup({
-    provider_selector = function(bufnr, filetype, buftype)
-        -- use indent only HERE. to avoid treesitter error
-        return {'treesitter', 'indent'}
-        -- return {'indent'}
-    end
-})
